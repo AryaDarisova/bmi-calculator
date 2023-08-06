b4a9ad6578d951f3e8f6c6a84753189a5802c866
AryaDarisova <arisova.darya@yandex.ru>
2023-08-06 17:17:22 +0400

    Add app design

--- bmi-calculator/ViewController.swift
+++ bmi-calculator/ViewController.swift
@@ -9,11 +9,26 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var heightLabel: UILabel!
+    @IBOutlet weak var weightLabel: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func heightSliderChanged(_ sender: UISlider) {
+        let height = String(Float(round(sender.value * 100) / 100))
+        
+        heightLabel.text = "\(height)m"
+    }
+    
+    @IBAction func weightSliderChanged(_ sender: UISlider) {
+        let weight = String(Float(round(sender.value * 10) / 10))
+        weightLabel.text = "\(weight)Kg"
+    }
+    
+    @IBAction func calculatePressed(_ sender: UIButton) {
+//        let bmi = weight / pow(height, 2) (height * height)
+    }
 }
 
